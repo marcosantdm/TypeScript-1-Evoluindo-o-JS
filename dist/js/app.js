1e70887a4f8e8a300d58d81661b587cbe3c70ac0
Marcos Antonio de Lima Claudino <marcos.antoniodm039@gmail.com>
2024-01-09 17:06:23 -0300

    feat: create getters

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,8 @@
-alert('oi');
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 10, 100)
+
+console.log(negociacao.data);
+console.log(negociacao.quantidade);
+console.log(negociacao.valor);
+console.log(negociacao.volume);
